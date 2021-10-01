b5af3a6c898f802fffa65bd459d4c1ec96f4c19a
Andrew Piatetsky <apiatetsky30@stuy.edu>
2021-09-30 20:23:55 -0400

    initial setup

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,19 @@
+public class Greet {
+        public static void main(String[] args){
+                greet("Why, hello there, Foo. How do you do?");
+                System.out.println("No hablo queso");
+        }
+
+        public static void greet(String x) {
+                System.out.println(x);
+        }
+}
+
+/*
+What does it mean to hardcode 3 people. What people, how do you hardcode someone?
+Does he want us to have 3 options and they are picked randomly? or does he want us to just
+pick one out of 3 names?
+*/
+
+//Emails - mkamela20
+//xlin30
