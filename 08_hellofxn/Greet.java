385878097d80899ac2f59d62326a4ee123c50c9e
Andrew Piatetsky <apiatetsky30@stuy.edu>
2021-10-03 12:50:18 -0400

    initial setup

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,5 +1,4 @@
 public class Greet {
-<<<<<<< HEAD
 	public static void main(String[] args){
 		greet("Why, hello there, Foo. How do you do?");
 		System.out.println("No hablo queso");
@@ -33,7 +32,4 @@ pick one out of 3 names?
 
 //Emails - mkamela20
 //xlin30
-<<<<<<< HEAD
 
-=======
->>>>>>> b668b235bebe51d11a5f1aaac3e3ae3273590197
