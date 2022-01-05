447ae119610472b6db36b8b06daa60711b946499
Andrew Piatetsky <apiatetsky30@stuy.edu>
2022-01-04 19:53:01 -0500

    initial commit

--- 52_selectionsort/SelectionSort.java
+++ 52_selectionsort/SelectionSort.java
@@ -1,25 +1,31 @@
-// Clyde "Thluffy" Sinclair
-// APCS pd0
-// HW52 -- implementing selection sort
-// 2022-01-05w
-// time spent:  hrs
+/*
+TNPG: Blue Pizza Bagles (Andrew Piatesky, Ben Belotser, Kosta Dubovskiy) and Burnt Peanut
+APCS
+HW52 - Selection, Natch/Sorting algos/created a not so memory intensive sorting algorithm called selection sort which searches array for smallest item, places it at the beginning and then repeats for next smallest item.
+2021-1-4
+time spent: 1hr
+*/
 
 /******************************
  *   class SelectionSort -- implements SelectionSort algorithm
  *
- * ALGO:
+ * ALGO: Our algorithm searches the entire array by comparing a smallest value to current value
+ *       Then it places smallest value at the beginning and starts looking for next smallest value
  *
- * DISCO
+ * DISCO:
+ *  I know we had an entire unit on this but I forgot that doing Array a = another array doesn't make a copy of another array, it points to it instead and changes it too
+ *  There are a lot of if statements and variables involved
+ *  In this case not much faster than bubble sort because does maximum amount of loops anyways
  *
  * QCC
  * q0: How many passes to sort n elements?
- * a0:
+ * a0: (n-1)! / n - 1 factorial
  * q1: What do you know after pass p?
- * a1:
+ * a1: that the first p elements are sorted
  * q2: How do you know if sorted?
- * a2:
- * q3: What does a pass boil down to?
- * a3:
+ * a2: Nothing is smaller than the last value in the array
+ * q3: What does a pass boil down to? (don't exactly understand the question)
+ * a3: a pass goes through the entire array scanning it, ends at the end of the array
  ******************************/
 
 
@@ -64,22 +70,44 @@ public class SelectionSort
   {
     //note: this version places smallest value at "rightmost" end
 
-    //maxPos will point to position of SELECTION (smallest value)
-    int minPos;
+    Comparable minVal; // smallest value 
+    Comparable curVal; // current value at position z
 
     for(int i = 0; i < data.size(); i++) {
-      System.out.println( "\nbegin pass " + (data.size()-i) );//diag
+      //System.out.println( "\nbegin pass " + (i) );//diag
+      //System.out.println(data);
 
+      minVal = data.get(i);
 
-      for(int z = i +1 ; z < data.size(); z++) {
-        System.out.println( "maxPos: " + maxPos );//diag
-        System.out.println( data );//diag
+      // nested forloop
+      for(int z = i + 1 ; z < data.size(); z++) {
 
+        curVal = data.get(z);
 
-      }
+        // System.out.println( "minPos: " + minPos + " minVal: " + minVal );//diag
+        // System.out.println( data );//diag
 
+        // when at the end of an array
+        if (z == data.size()-1) {
+          // System.out.println("End?" + z);
+          // when minVal is greater than curVal, minVal takes the value of curVal
+          if (minVal.compareTo(curVal) > 0) {
+            minVal = curVal;
+          }
+        } // end last value of list cases
+        else {
+          // when minVal is greater than curVal, minVal takes the value of curVal 
+          if (minVal.compareTo(curVal) > 0) {
+            minVal = curVal;
+          }
+        } // end general cases
 
-      System.out.println( "after swap: " +  data );//diag
+      } // end nested forloop
+
+      data.remove(minVal);
+      data.add(i, minVal);
+
+      // System.out.println( "after swap: " +  data );//diag
     }
   }//end selectionSort
 
@@ -106,7 +134,7 @@ public class SelectionSort
   public static void main( String [] args )
   {
 
-    /*===============for VOID methods=============
+    /*===============for VOID methods=============*/
     ArrayList glen = new ArrayList<Integer>();
     glen.add(7);
     glen.add(1);
@@ -121,10 +149,10 @@ public class SelectionSort
     System.out.println( "ArrayList coco before sorting:\n" + coco );
     selectionSortV(coco);
     System.out.println( "ArrayList coco after sorting:\n" + coco );
-      ============================================*/
+      /*============================================*/
 
-    /*==========for AL-returning methods==========
-      ArrayList glen = new ArrayList<Integer>();
+    /*==========for AL-returning methods==========*/
+      glen = new ArrayList<Integer>();
       glen.add(7);
       glen.add(1);
       glen.add(5);
@@ -134,16 +162,15 @@ public class SelectionSort
       ArrayList glenSorted = selectionSort( glen );
       System.out.println( "sorted version of ArrayList glen:\n"
       + glenSorted );
-      System.out.println( "ArrayList glen after sorting:\n" + glen );
+      System.out.println( "ArrayList glen after sorting:\n" + glen ); // shouldn't be sorted because manually copied arraylist
 
-      ArrayList coco = populate( 10, 1, 1000 );
+      coco = populate( 10, 1, 1000 );
       System.out.println( "ArrayList coco before sorting:\n" + coco );
       ArrayList cocoSorted = selectionSort( coco );
       System.out.println( "sorted version of ArrayList coco:\n"
       + cocoSorted );
       System.out.println( "ArrayList coco after sorting:\n" + coco );
-      System.out.println( coco );
-      ============================================*/
+      /*============================================*/
 
   }//end main
 
