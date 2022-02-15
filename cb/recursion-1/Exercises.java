e8ed5880115371a9cbbe07a794eefaafe56e870f
Andrew Piatetsky <apiatetsky30@stuy.edu>
2022-02-14 20:54:47 -0500

    did some more cb

--- cb/recursion-1/Exercises.java
+++ cb/recursion-1/Exercises.java
@@ -1,3 +1,10 @@
+// TNPG - 16 year streak (Andrew Piatetsky and Ben Belotser)
+// APCS pd6
+// HW64: Revisitation
+// 2022-02-14
+// time spent: in class + 30min
+
+
 public class Exercises {
 
 
@@ -23,20 +30,48 @@ public class Exercises {
             int sum = bunnyEars(bunnies-1) + 2;
             return sum;
         }        
-    }       
- public int fibonacci(int n) {
-  
-  if (n == 0) {
-    return 0;
-  }
-  if (n == 1) {
-    return 1;
-  }
-  else {
-    int sum = fibonacci(n-2)+fibonacci(n-1);
-    return sum;
-  }
-}
+    }
+    
+    // fib
+    public int fibonacci(int n) {
+      
+      if (n == 0) {
+        return 0;
+      }
+      if (n == 1) {
+        return 1;
+      }
+      else {
+        int sum = fibonacci(n-2)+fibonacci(n-1);
+        return sum;
+      }
+    }
+
+    // bunny ears 2
+    public int bunnyEars2(int bunnies) {
+      int sum;
+      if (bunnies == 0) {
+        return 0;
+      }
+      if (bunnies % 2 == 0) {
+        sum = 3 + bunnyEars2(bunnies-1);
+      }
+      else {
+        sum = 2 + bunnyEars2(bunnies-1);
+      }
+      return sum;
+    }
+
+    // sum of triangle blocks
+    public int triangle(int rows) {
+      if (rows == 0) {
+        return 0;
+      }
+      else {
+        int total = rows + triangle(rows-1);
+        return total;
+      }
+    }
 
 
 }
---
+++ cb/recursion-2/Exercises.java
@@ -0,0 +1,16 @@
+// didn't do any :(, but valentine and I argued 
+
+// TNPG - 16 year streak (Andrew Piatetsky and Ben Belotser)
+// APCS pd6
+// HW64: Revisitation
+// 2022-02-14
+// time spent: in class + 30min
+
+/* 
+QCC
+How do you do this. Since the function returns a boolean how do you go through the list, nothing to return and nothing to check except length and  start point.
+
+
+
+
+*/
