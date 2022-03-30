b60521262662615132815ecd8283a1a2654c6f86
Andrew Piatetsky <apiatetsky30@stuy.edu>
2022-03-29 21:31:34 -0400

    added 84 skeleton

---
+++ 84_stack/LatKtS.java
@@ -0,0 +1,63 @@
+import java.util.Stack;
+
+/***
+ * class LatKtS
+ * SKELETON
+ * driver class for Latkes
+ * Uses a stack to reverse a text string, check for sets of matching parens.
+ **/
+
+public class LatKtS
+{
+
+  /***
+   * precondition:  input string has length > 0
+   * postcondition: returns reversed string s
+   *                flip("desserts") -> "stressed"
+   **/
+  public static String flip( String s )
+  {
+    Latkes stack = new Latkes(s.length());
+    String retStr = "";
+
+    for (int i = 0; i < s.length(); i++) {
+        stack.push(s.substring(i, i+1));
+    }
+
+    while (!stack.isEmpty()) {
+        retStr += stack.pop();
+    }
+
+    return retStr;
+  }
+
+  /***
+   * precondition:  s contains only the characters {,},(,),[,]
+   * postcondition: allMatched( "({}[()])" )    -> true
+   *                allMatched( "([)]" )        -> false
+   *                allMatched( "" )            -> true
+   **/
+  public static boolean allMatched( String s )
+  {
+    
+    return true;
+  }
+
+
+  //main method to test
+  public static void main( String[] args )
+  {
+
+      
+      System.out.println(flip("racecar"));
+    /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+    System.out.println(allMatched( "({}[()])" )); //true
+    System.out.println(allMatched( "([)]" ) ); //false
+    System.out.println(allMatched( "(){([])}" ) ); //true
+    System.out.println(allMatched( "](){([])}" ) ); //false
+    System.out.println(allMatched( "(){([])}(" ) ); //false
+    System.out.println(allMatched( "()[[]]{{{{((([])))}}}}" ) ); //true
+      ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
+  }
+
+}//end class
