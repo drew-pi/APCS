11c2a8e70dcafb10e2f42b43320d0e68cc95820b
Andrew Piatetsky <apiatetsky30@stuy.edu>
2022-04-01 12:33:24 -0400

    added files

---
+++ 87_queue/DLLNode.java
@@ -0,0 +1,71 @@
+/*****************************************************
+ * class DLLNode
+ * Implements a node, for use in lists and other container classes.
+ *****************************************************/
+
+public class DLLNode<T> {
+
+  private T _cargo;    //cargo may only be of type T
+  private DLLNode<T> _nextNode; //pointers to next, prev DLLNodes
+
+
+  // constructor -- initializes instance vars
+  public DLLNode( T value, DLLNode<T> next )
+{
+_cargo = value;
+_nextNode = next;
+  }
+
+
+  //--------------v  ACCESSORS  v--------------
+  public T getCargo() { return _cargo; }
+
+  public DLLNode<T> getNext() { return _nextNode; }
+
+  //--------------^  ACCESSORS  ^--------------
+
+
+  //--------------v  MUTATORS  v--------------
+  public T setCargo( T newCargo ) {
+T foo = getCargo();
+_cargo = newCargo;
+return foo;
+  }
+
+  public DLLNode<T> setNext( DLLNode<T> newNext ) {
+DLLNode<T> foo = getNext();
+_nextNode = newNext;
+return foo;
+  }
+
+  //--------------^  MUTATORS  ^--------------
+
+
+  // override inherited toString
+  public String toString() { return _cargo.toString(); }
+
+
+  //main method for testing
+  public static void main( String[] args )
+{
+//Below is an exercise in creating a linked list...
+
+/*********************
+//Create a node
+DLLNode<String> first = new DLLNode<String>( "cat", null );
+
+//Create a new node after the first
+first.setNext( new DLLNode<String>( "dog", null ) );
+
+//Create a third node after the second
+first.getNext().setNext( new DLLNode<String>( "cow", null ) );
+
+DLLNode temp = first;
+while( temp != null ) {
+    System.out.println( temp );
+    temp = temp.getNext();
+}
+***********************/
+  }//end main
+
+}//end class DLLNode
---
+++ 87_queue/NodeQueue.java
@@ -0,0 +1,26 @@
+
+
+
+public class NodeQueue<QUASAR> implements Queue<QUASAR>{
+
+
+  public QUASAR dequeue() {
+
+  }
+
+  //means of adding an element to collection:
+  //Enqueue an element onto the back of this queue.
+  public void enqueue( QUASAR x ) {
+
+  }
+
+  //Returns true if this queue is empty, otherwise returns false.
+  public boolean isEmpty() {
+    return true;
+  }
+
+  //Returns the first element of the queue without dequeuing it.
+  public QUASAR peekFront() {
+    
+  }
+}
